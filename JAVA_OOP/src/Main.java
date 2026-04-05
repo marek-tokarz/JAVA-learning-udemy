460c6b8aa6f14a8d5d8e4ab0fb7fcb7be3a25a0b
Marek Tokarz <marek.tokarz14@gmail.com>
2026-04-05 20:04:39 +0200

    Defalut values for car and getters for car

--- JAVA_OOP/src/Main.java
+++ JAVA_OOP/src/Main.java
@@ -2,6 +2,8 @@ public class Main{
     public static void main(String[] args)
     {
       Car car = new Car();
+      System.out.println("make = " + car.getMake());
+      System.out.println("model = " + car.getModel());
       car.describeCar();
     }
 }
