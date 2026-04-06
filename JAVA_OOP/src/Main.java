aa96bc112131ecba79a62c4628480e0d4fcd7ee8
Marek Tokarz <marek.tokarz14@gmail.com>
2026-04-06 12:55:22 +0200

    Record type, creating objects in a loop - of type record

--- JAVA_OOP/src/Main.java
+++ JAVA_OOP/src/Main.java
@@ -15,8 +15,20 @@ public class Main{
       car.describeCar();
       */
 
-        Account bobsAccount = new Account("12345", 1000.00, "Bob Brown", "myemail@bob.com", "(087) 123-4567");
-
+        Account bobsAccount = new Account(
+                "12345", 1000.00, "Bob Brown", "myemail@bob.com", "(087) 123-4567");
+        for(int i = 1; i <= 5; i++) {
+            LPAStudent s = new LPAStudent("S92300" + i,
+                    switch(i) {
+                        case 1 -> "Mary";
+                        case 2 -> "Carol";
+                        case 3 -> "Tim";
+                        case 4 -> "Lisa";
+                        default -> "Anonymous";
+                    }, "05/11/1985",
+                    "Java Masterclass");
+            System.out.println(s);
+        }
     }
 }
 
