a74e48e8ba8927ebacf006270ac692ef7cd3bc2c
Marek Tokarz <marek.tokarz14@gmail.com>
2026-04-06 07:54:11 +0200

    Setters for car and call for setters of car

--- JAVA_OOP/src/Main.java
+++ JAVA_OOP/src/Main.java
@@ -2,6 +2,13 @@ public class Main{
     public static void main(String[] args)
     {
       Car car = new Car();
+
+      car.setMake("Porsche");
+      car.setModel("Carrera");
+      car.setDoors(2);
+      car.setConvertible(true);
+      car.setColor("Black");
+      
       System.out.println("make = " + car.getMake());
       System.out.println("model = " + car.getModel());
       car.describeCar();
