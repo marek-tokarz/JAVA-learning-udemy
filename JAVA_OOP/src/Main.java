ee835aa614d5d7afba731879268acf8b90524f0b
marek-tokarz <marek.tokarz14@gmail.com>
2026-04-12 13:59:16 +0200

    String class and its method, StringBuilder class and its methods and moved .gitignore to a higher folder

--- JAVA_OOP/src/Main.java
+++ JAVA_OOP/src/Main.java
@@ -39,7 +39,7 @@ public class Main{
         System.out.println(student.name() + " is taking " + student.classList());
 
         // record is immutable
-        */
+
 
         Animal animal = new Animal("Generic Animal", "Huge", 400);
         doAnimalStuff(animal, "slow");
@@ -57,7 +57,144 @@ public class Main{
 
         Fish goldie = new Fish("Goldfish", 0.25, 2, 3);
         doAnimalStuff(goldie, "fast");
+
+
+        // Text blocks and advanced formatting
+        String bulletIt = "Print a Bulleted List:\n" +
+                "\t\u2022 First Point\n" +
+                "\t\t\u2022 Sub Point\n";
+
+        System.out.println(bulletIt);
+
+        String textBlock = """
+                Print a Bulleted List:
+                        \u2022 First Point
+                            \u2022 Sub Point""";
+
+        System.out.println(textBlock);
+
+        int age = 35;
+        System.out.printf("Your age is %d%n", age);
+
+        int yearOfBirth = 2023 - age;
+        System.out.printf("Age = %d, Birth year = %d%n", age, yearOfBirth);
+
+        System.out.printf("Your age is %.2f%n", (float) age);
+
+        for(int i = 1; i < 100000; i *= 10) {
+            System.out.printf("Printing %6d %n",i);
+        }
+
+        String formattedString = String.format("Your age is %d", age);
+        System.out.println(formattedString);
+
+        formattedString = "Your age is %d".formatted(age);
+        System.out.println(formattedString);
+
+        printInformation("Hello World");
+        printInformation("");
+        printInformation("\t   \n");
+        String helloWorld = "Hello World";
+        System.out.printf("index of r = %d %n", helloWorld.indexOf('r'));
+        System.out.printf("index of World = %d %n", helloWorld.indexOf("World"));
+
+        System.out.printf("index of r = %d %n", helloWorld.indexOf('l'));
+        System.out.printf("index of r = %d %n", helloWorld.lastIndexOf('l'));
+
+        System.out.printf("index of r = %d %n", helloWorld.indexOf('l',3));
+        System.out.printf("index of r = %d %n", helloWorld.lastIndexOf('l',8));
+
+        // String class methods - inspecting and comparing
+        String helloWolrdLower = helloWorld.toLowerCase();
+        if(helloWorld.equals(helloWolrdLower)) {
+            System.out.println("Values match exactly");
+        }
+
+        if(helloWorld.equalsIgnoreCase(helloWolrdLower)) {
+            System.out.println("Values match ignoring case");
+        }
+
+        // String class methods - manipulating
+
+        String birthDate = "25/11/1982";
+        int startingIndex = birthDate.indexOf("1982");
+        System.out.println("StartingIndex = " + startingIndex);
+        System.out.println("Birth year = " + birthDate.substring(startingIndex));
+        System.out.println("Month = " + birthDate.substring(3,5));
+
+        String newDate = String.join("/", "25", "11", "1982");
+        System.out.println("newDate " + newDate );
+
+        newDate = "25" + "/" + "11" + "/" + "1982";
+        System.out.println("newDate " + newDate );
+
+        System.out.println(newDate.replace('/', '-') );
+        System.out.println(newDate.replace("2", "00"));
+
+        System.out.println(newDate.replace("/", "-") );
+        System.out.println(newDate.replace("/", "---"));
+
+        System.out.println("ABC\n".repeat(3));
+        System.out.println("-".repeat(20));
+
+        System.out.println("ABC\n".repeat(3).indent(8));
+        System.out.println("-".repeat(20));
+
+        System.out.println("      ABC\n".repeat(3).indent(-2));
+        System.out.println("-".repeat(20));
+
+         */
+
+        // StringBuilder
+
+        String helloWorld = "Hello" + " World";
+        helloWorld.concat(" and Goodbye"); // will not change an above String
+
+        StringBuilder helloWorldBuilder = new StringBuilder("Hello" + " World");
+        helloWorldBuilder.append(" and Goodbye");
+
+        printInformation(helloWorld);
+        printInformation(helloWorldBuilder);
+
+        StringBuilder emptyStart = new StringBuilder();
+        emptyStart.append("a".repeat(17));
+
+        StringBuilder emptyStart32 = new StringBuilder(32);
+        emptyStart32.append("a".repeat(17));
+
+        printInformation(emptyStart);
+        printInformation(emptyStart32);
     }
+
+    public static void printInformation(String string) {
+        System.out.println("String = " + string);
+        System.out.println("length = " + string.length());
+    }
+
+    public static void printInformation(StringBuilder builder) {
+        System.out.println("String = " + builder);
+        System.out.println("length = " + builder.length());
+        System.out.println("capacity = " + builder.capacity());
+    }
+
+    /*
+    public static void printInformation(String string) {
+        int length = string.length();
+        System.out.printf("Length = %d %n", length);
+
+        if(string.isEmpty()) {
+            System.out.println("String is Empty");
+            return;
+        }
+
+        if(string.isBlank()) {
+            System.out.println("String is Blank");
+        }
+
+        System.out.printf("First char = %c %n", string.charAt(0));
+        System.out.printf("Lat character = %c %n", string.charAt(length-1));
+    }
+      */
 
     public static void doAnimalStuff(Animal animal, String speed) {
 
