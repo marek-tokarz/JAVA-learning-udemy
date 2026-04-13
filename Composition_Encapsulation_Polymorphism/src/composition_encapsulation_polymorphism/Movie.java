d4152f161463d0674c0724fe49316767dfc72107
marek-tokarz <marek.tokarz14@gmail.com>
2026-04-13 13:01:50 +0200

    Add class Movie

---
+++ Composition_Encapsulation_Polymorphism/src/composition_encapsulation_polymorphism/Movie.java
@@ -0,0 +1,15 @@
+package composition_encapsulation_polymorphism;
+
+public class Movie {
+
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public void watchMovie() {
+        String instanceType = this.getClass().getSimpleName();
+        System.out.println(title + " is a " + instanceType + " film");
+    }
+}
