4d563777bb75ac72077868ec54b7f7fac9736eb9
marek-tokarz <marek.tokarz14@gmail.com>
2026-04-13 16:02:36 +0200

    Added a subclasses of a Car class, classes: ElectricCar, HybridCar

--- Composition_Encapsulation_Polymorphism/src/composition_encapsulation_polymorphism/Car.java
+++ Composition_Encapsulation_Polymorphism/src/composition_encapsulation_polymorphism/Car.java
@@ -31,9 +31,9 @@ class GasPoweredCar extends Car {
         super(description);
     }
 
-    public GasPoweredCar(String description, double acgKmPerLiter, int cylinders) {
+    public GasPoweredCar(String description, double avgKmPerLiter, int cylinders) {
         super(description);
-        this.avgKmPerLiter = acgKmPerLiter;
+        this.avgKmPerLiter = avgKmPerLiter;
         this.cylinders = cylinders;
     }
 
@@ -45,5 +45,61 @@ class GasPoweredCar extends Car {
     @Override
     protected void runEngine() {
         System.out.printf("Gas -> usage exceeds the average: %.2f %n", avgKmPerLiter);
+    }
+}
+
+class ElectricCar extends Car {
+
+    private double avgKmPerCharge;
+    private int batterySize = 6;
+
+    public ElectricCar(String description) {
+        super(description);
+    }
+
+    public ElectricCar(String description, double acgKmPerLiter, int cylinders) {
+        super(description);
+        this.avgKmPerCharge = acgKmPerLiter;
+        this.batterySize = cylinders;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("BEV -> switch %d kWh battery on, Ready!%n",batterySize);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("BEV -> usage under the average: %.2f %n", avgKmPerCharge);
+    }
+}
+
+class HybridCar extends Car {
+
+    private double avgKmPerLiter;
+    private int cylinders = 6;
+
+    private int batterySize;
+
+    public HybridCar(String description) {
+        super(description);
+    }
+
+    public HybridCar(String description, double avgKmPerLiter, int cylinders, int batterySize) {
+        super(description);
+        this.avgKmPerLiter = avgKmPerLiter;
+        this.cylinders = cylinders;
+        this.batterySize = cylinders;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("Hybrid -> %d cylinders are fired up! %n", cylinders);
+        System.out.printf("Hybrid -> switch %d kWh battery on, Ready! %n", batterySize);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("Hybrid -> usage below average: %.2f %n", avgKmPerLiter);
     }
 }
