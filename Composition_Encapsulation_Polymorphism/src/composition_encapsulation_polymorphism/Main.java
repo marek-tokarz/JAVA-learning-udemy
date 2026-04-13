23d4d8529ae7c83100b83db5153fe8d79574fb89
marek-tokarz <marek.tokarz14@gmail.com>
2026-04-13 18:45:36 +0200

    MealOrder instances and its methods

--- Composition_Encapsulation_Polymorphism/src/composition_encapsulation_polymorphism/Main.java
+++ Composition_Encapsulation_Polymorphism/src/composition_encapsulation_polymorphism/Main.java
@@ -110,8 +110,19 @@ public class Main {
 //        Item avocado = new Item ("Topping", "avocado", 1.50);
 //        avocado.printItem();
 
-        Burger burger = new Burger("regular", 4.00);
-        burger.addToppings("BACON", "CHEESE", "MAYO");
-        burger.printItem();
+//        Burger burger = new Burger("regular", 4.00);
+//        burger.addToppings("BACON", "CHEESE", "MAYO");
+//        burger.printItem();
+
+        MealOrder regularMeal = new MealOrder();
+        regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+        regularMeal.setDrinkSize("LARGE");
+        regularMeal.printItemizedList();
+
+        MealOrder secondMeal = new MealOrder("turkey", "7-up",
+                "chili");
+        secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
+        secondMeal.setDrinkSize("SMALL");
+        secondMeal.printItemizedList();
     }
 }
